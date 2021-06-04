a15dc1dbc1aa04e6aecf162c34107f881bfc4193
hgliyuhao <1241225413@qq.com>
2021-06-04 15:15:23 +0800

    update code

--- ner_train.py
+++ ner_train.py
@@ -16,15 +16,14 @@ import fairies as fa
 from bert4keras.backend import keras, search_layer, K
 
 maxlen = 256
-epochs = 30
-batch_size = 4
+epochs = 20
+batch_size = 16
 learning_rate = 1e-5
 categories = set()
 
 def load_data(filename):
     
-    """
-        加载数据
+    """加载数据
         单条格式：[text, (start, end, label), (start, end, label), ...]，
                   意味着text[start:end + 1]是类型为label的实体。
     """
@@ -33,38 +32,45 @@ def load_data(filename):
     data = fa.read_json(filename)
 
     for d in data:
-        text= d['text']
-        if 'event_list' not in d:
-            continue
-        else:
-            event_lists = d['event_list']
-            
+        text = d['text']
+        spo_list = d['spo_list']
+
         entry_list = []
         entry_list.append(text)
 
-        for event_list in event_lists:
-            event_type = event_list['event_type']
-            arguments = event_list['arguments']
-            for argument in arguments:
-                argument_type = event_type + '_' + argument['role']
-                categories.add(argument_type)
-                argument = argument['argument']
-                
-                if text.find(argument) != -1:
-                    entry_list.append([text.find(argument),text.find(argument)+len(argument)-1,argument_type])
+        for s in spo_list:
+             
+            predicate = s['predicate']
+            subject_type = predicate + 's_' + s['subject_type']
+            categories.add(subject_type)
+            subject = s['subject']
+            if text.find(subject) != -1:
+                entry_list.append([text.find(subject),text.find(subject)+len(subject)-1,subject_type])
+
+            for ob in s['object']:
+                ob_ject_type = predicate + 'o_' + s['object_type'][ob]
+                categories.add(ob_ject_type)
+                ob_ject = s['object'][ob]
+                if text.find(ob_ject) != -1:
+                    entry_list.append([text.find(ob_ject),text.find(ob_ject)+len(ob_ject)-1,ob_ject_type])
         
         D.append(entry_list)
-    
+
     return D
 
-train_data = load_data('duee_train.json')
-valid_data = load_data('duee_dev.json')
+# 标注数据
+train_data = load_data('duie_train.json')
+valid_data = load_data('duie_dev.json')
 
+# test_data = load_data('duie_test.json')
 categories = list(sorted(categories))
+print(categories)
+print(len(categories))
 
-config_path = 'roeberta_zh_L-24_H-1024_A-16/bert_config_large.json'
-checkpoint_path = 'roeberta_zh_L-24_H-1024_A-16/roberta_zh_large_model.ckpt'
-dict_path = 'roeberta_zh_L-24_H-1024_A-16/vocab.txt'
+# 建立分词器
+config_path = 'chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = 'chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = 'chinese_roberta_wwm_ext_L-12_H-768_A-12/vocab.txt'
 tokenizer = Tokenizer(dict_path, do_lower_case=True)
 
 class data_generator(DataGenerator):
@@ -114,80 +120,6 @@ def global_pointer_f1_score(y_true, y_pred):
     y_pred = K.cast(K.greater(y_pred, 0), K.floatx())
     return 2 * K.sum(y_true * y_pred) / K.sum(y_true + y_pred)
 
-model = build_transformer_model(
-    config_path=config_path,
-    checkpoint_path=checkpoint_path,
-    return_keras_model=False,
-) 
-
-output = GlobalPointer(len(categories), 64)(model.output)
-
-model = Model(model.input, output)
-model.summary()
-
-model.compile(
-    loss=global_pointer_crossentropy,
-    optimizer=Adam(learning_rate),
-    metrics=[global_pointer_f1_score]
-)
-
-class NamedEntityRecognizer(object):
-    """命名实体识别器
-    """
-    def recognize(self, text, threshold=0):
-        tokens = tokenizer.tokenize(text, maxlen=256)
-        mapping = tokenizer.rematch(text, tokens)
-        token_ids = tokenizer.tokens_to_ids(tokens)
-        segment_ids = [0] * len(token_ids)
-        token_ids, segment_ids = to_array([token_ids], [segment_ids])
-        scores = model.predict([token_ids, segment_ids])[0]
-        scores[:, [0, -1]] -= np.inf
-        scores[:, :, [0, -1]] -= np.inf
-        entities = []
-        for l, start, end in zip(*np.where(scores > threshold)):
-            entities.append(
-                (mapping[start][0], mapping[end][-1], categories[l])
-            )
-        return entities
-
-NER = NamedEntityRecognizer()
-
-def evaluate(data):
-    """评测函数
-    """
-    X, Y, Z = 1e-10, 1e-10, 1e-10
-    for d in tqdm(data, ncols=100):
-        R = set(NER.recognize(d[0]))
-        T = set([tuple(i) for i in d[1:]])
-        X += len(R & T)
-        Y += len(R)
-        Z += len(T)
-    f1, precision, recall = 2 * X / (Y + Z), X / Y, X / Z
-    return f1, precision, recall
-
-
-class Evaluator(keras.callbacks.Callback):
-    """评估与保存
-    """
-    
-    def __init__(self):
-        self.best_val_f1 = 0
-
-    def on_epoch_end(self, epoch, logs=None):
-
-        model.save_weights('last_model.weights')
-        f1, precision, recall = evaluate(valid_data)
-        
-        # 保存最优
-        if f1 >= self.best_val_f1:
-            self.best_val_f1 = f1
-            model.save_weights('best_model.weights')
-        print(
-            'valid:  f1: %.5f, precision: %.5f, recall: %.5f, best f1: %.5f\n' %
-            (f1, precision, recall, self.best_val_f1)
-        )
-
-
 def adversarial_training(model, embedding_name, epsilon=1):
     """给模型添加对抗训练
     其中model是需要添加对抗训练的keras模型，embedding_name
@@ -231,14 +163,85 @@ def adversarial_training(model, embedding_name, epsilon=1):
     model.train_function = train_function  # 覆盖原训练函数
 
 
+model = build_transformer_model(
+    config_path=config_path,
+    checkpoint_path=checkpoint_path,
+    return_keras_model=False,
+) 
+
+output = GlobalPointer(len(categories), 64)(model.output)
+
+model = Model(model.input, output)
+model.summary()
+
+model.compile(
+    loss=global_pointer_crossentropy,
+    optimizer=Adam(learning_rate),
+    metrics=[global_pointer_f1_score]
+)
+
+class NamedEntityRecognizer(object):
+    """命名实体识别器
+    """
+    def recognize(self, text, threshold=0):
+        tokens = tokenizer.tokenize(text, maxlen=512)
+        mapping = tokenizer.rematch(text, tokens)
+        token_ids = tokenizer.tokens_to_ids(tokens)
+        segment_ids = [0] * len(token_ids)
+        token_ids, segment_ids = to_array([token_ids], [segment_ids])
+        scores = model.predict([token_ids, segment_ids])[0]
+        scores[:, [0, -1]] -= np.inf
+        scores[:, :, [0, -1]] -= np.inf
+        entities = []
+        for l, start, end in zip(*np.where(scores > threshold)):
+            entities.append(
+                (mapping[start][0], mapping[end][-1], categories[l])
+            )
+        return entities
+
+NER = NamedEntityRecognizer()
+
+def evaluate(data):
+    """评测函数
+    """
+    X, Y, Z = 1e-10, 1e-10, 1e-10
+    for d in tqdm(data, ncols=100):
+        R = set(NER.recognize(d[0]))
+        T = set([tuple(i) for i in d[1:]])
+        X += len(R & T)
+        Y += len(R)
+        Z += len(T)
+    f1, precision, recall = 2 * X / (Y + Z), X / Y, X / Z
+    return f1, precision, recall
+
+class Evaluator(keras.callbacks.Callback):
+    """评估与保存
+    """
+    
+    def __init__(self):
+        self.best_val_f1 = 0
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        model.save_weights('last_model.weights')
+        f1, precision, recall = evaluate(valid_data)
+        
+        # 保存最优
+        if f1 >= self.best_val_f1:
+            self.best_val_f1 = f1
+            model.save_weights('best_model.weights')
+        print(
+            'valid:  f1: %.5f, precision: %.5f, recall: %.5f, best f1: %.5f\n' %
+            (f1, precision, recall, self.best_val_f1)
+        )
+       
 
 if __name__ == '__main__':
 
     evaluator = Evaluator()
     train_generator = data_generator(train_data, batch_size)
     
-    model.load_weights('best_model.weights')
-    adversarial_training(model,'Embedding-Token',0.5)
+    adversarial_training(model, 'Embedding-Token', 0.5)
     
     model.fit(
         train_generator.forfit(),
@@ -246,19 +249,20 @@ if __name__ == '__main__':
         epochs=epochs,
         callbacks=[evaluator]
     )
-    
-    test_data = fa.read_json('duee_test2.json')
-    
-    res = []
-    
-    for i in tqdm(test_data, ncols=100):    
-        text = i['text']
-        R = set(NER.recognize(text))
-        i['event_list'] = list(R)
-        res.append(i)
-    
-    
-    fa.write_json('test2.json',res)
-    
         
-    
+    model.load_weights('81_best_model.weights')
+
+    test_data = fa.read_json('duie_test2.json')
+        
+    entry_new_res_512 = []
+    for t in tqdm(test_data, ncols=100):
+#     for t in test_data:
+        text = t['text']
+        R = set(NER.recognize(text))
+        entry_list = []
+        for i in R:
+            entry_list.append([text[i[0]:i[1]+1],i[2]])
+#         print(entry_list)
+        entry_new_res_512.append([text,entry_list])
+        
+    fa.write_json('81_entry_duie_test2.json',entry_new_res_512)
